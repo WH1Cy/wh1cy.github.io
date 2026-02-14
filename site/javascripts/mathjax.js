1a7c8bc7e5e0c5e69fca40ac12db1331f55e76b8
WH1Cy <whicy1823@163.com>
2026-02-14 19:39:57 +0800

    Update sitemap.xml with new last modified dates for all URLs to reflect recent content changes. Adjust course documentation in multiple lab reports for improved clarity and consistency in formatting, including mathematical expressions and section headings.

--- site/javascripts/mathjax.js
+++ site/javascripts/mathjax.js
@@ -1,9 +1,19 @@
 window.MathJax = {
+  loader: { load: ["[tex]/unicode"] },
   tex: {
+    packages: { "[+]": ["unicode"] },
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    macros: {
+      degree: "^{\\circ}",
+      degreeCelsius: "^{\\circ}\\mathrm{C}",
+      lt: "<",
+      gt: ">",
+      bold: ["\\mathbf{#1}", 1],
+      oiint: "\\mathop{\\unicode{x222F}}\\nolimits"
+    }
   },
   options: {
     ignoreHtmlClass: ".*|",
